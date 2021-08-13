216f4cd65afe6c64a7447b9ab25350bf15de4b93
geovanysan <geovanny1santiago@gmail.com>
2021-08-12 23:46:49 -0500

    poke

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import { createStore } from "redux";
 import reducer from './redux/reducers';
-const store = createStore(reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(reducer)
 
 ReactDOM.render(
   <Provider store={store} >
